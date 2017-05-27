806653abcb8b6dd164850c6ec08c607f556ab98b
Juan Ignacio Gil Gómez <iwan.manjak@gmail.com>
2017-05-27 18:56:19 +0200

    Revert "Trying (without success) to dynamically generate divs"
    This reverts commit b1f1879a9201911fa58304fa54f99f8510dd485b.

--- src/filldata.js
+++ src/filldata.js
@@ -2,16 +2,17 @@
     "use strict";
 
     var data = {
-      "Name": "Adolfo Trigonegro",
-      "Player": "Quinta",
-      "Chronicle": "Firenze, 1923"
+      "name": "Adolfo Trigonegro",
+      "player": "Quinta",
+      "children": [{
+        "id":"0.9685","name":" contents:queue"},{
+        "id":"0.79281","name":" contents:mqq_error"}]
     }
 
     $(function fillData() {
 
-        $.each(data, function(key, value) {
-          $(#description1).append("<div>" + key +": " + value "</div>");
-        });
+        document.getElementById("name").innerHTML = "<em>Nombre: " + data["name"] + "</em>";
+        document.getElementById("player").innerHTML = "<em>Player: " + data["player"] + "</em>";
       })
 
       // After the page finished loading, detect and
